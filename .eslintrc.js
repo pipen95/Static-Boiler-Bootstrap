a4769726ab38579dc0334ec16980785ca6ce86bd
Pierre Penel <penel.pierre@gmail.com>
2019-08-16 11:42:35 -0400

    My perfect Static Site  Build

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env": {
+      "browser": true,
+      "node": true,
+    
+    },
+    "parser": "babel-eslint",
+    "rules": {
+      "no-plusplus": 0,
+      "ts(8009)":0,
+      "eqeqeq":0,
+      "linebreak-style":0,
+      "import/no-extraneous-dependencies":0,
+      "no-param-reassign":0,
+      "no-undef":0,
+      "no-unused-vars":0
+
+  }
+};
---
+++ src/javascripts/scripts.js
@@ -0,0 +1,7 @@
+// Load Bootstrap from NPM
+import 'bootstrap';
+// Load jQuery from NPM
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
